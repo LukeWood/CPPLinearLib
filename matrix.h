843fd4917c33d6490d24f92a5a9c89cabe76218d
Luke Wood <lswood@smu.edu>
2016-02-22 12:19:06 -0600

    Comments

--- matrix.h
+++ matrix.h
@@ -7,9 +7,11 @@ namespace linear
 
 class matrix
 {
-
+	//Prints to std::ostream
 	friend ostream& operator<<(ostream&,const matrix&);
+	//Multiply Matrices
 	friend matrix operator*(const matrix&,const matrix&);
+	//Multiply Matrices by a Scalar
 	friend matrix operator*(double scalar, const matrix&);
 	
 		private:
@@ -17,23 +19,33 @@ class matrix
 			int height;
 			int width;	
 		public:
+			//Overloaded Copy Constructor
 			matrix(const matrix&);
+			//Basic Constructor Creating Matrix [0]
 			matrix();
+			//Creates a Zero Matrix of size specified;
 			matrix(int,int);
+			//Destructor
 			~matrix();
-
-			double* operator[](int) const;
-			matrix operator*(double) const;
-			matrix operator+(const matrix&) const;
-			matrix operator-(const matrix&) const;
-			matrix operator+(double) const;
-			matrix operator-(double) const;
-
+			//Assignment Operator
 			matrix operator=(const matrix&);
+			//operator to access a row	
+			double* operator[](int) const;
+			//Multiply by scalar
+			matrix operator*(double) const;
+			//Add to matrix
+			matrix operator+(const matrix&) const;
+			//Subtraction Matrix
+			matrix operator-(const matrix&) const;
+			//Add a double to ALL values in matrix
+			matrix operator+(double) const;
+			//subtract from ALL values in matrix
+			matrix operator-(double) const;
 			
 			int getHeight() const;
 			int getWidth() const;
 			
+			//Returns the dot product of the two matrices
 			double dot(const matrix&) const;
 			
 			//Transpose			
--- test.cpp
+++ test.cpp
@@ -5,9 +5,5 @@ using namespace linear;
 using std::cout;
 int main()
 {
-matrix m1(1,3);
-cout<<m1;
-m1 = m1.T();
-cout<<m1;
 return 0;
 }
